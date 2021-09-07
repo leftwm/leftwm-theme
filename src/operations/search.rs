85bb69196d86ef82dfd07a532d1e3ce3dcc7ad65
Sunny <darkowlzz@protonmail.com>
2021-09-07 18:42:17 +0530

    search: Improve the search matcher
    Introduces fuzzy matcher to improve the theme search.

--- src/operations/search.rs
+++ src/operations/search.rs
@@ -4,6 +4,8 @@ use clap::Clap;
 use colored::Colorize;
 use edit_distance::edit_distance;
 use errors::LeftError;
+use fuzzy_matcher::skim::SkimMatcherV2;
+use fuzzy_matcher::FuzzyMatcher;
 use log::trace;
 
 /* This function searches for themes, but does not update them by default
@@ -34,7 +36,7 @@ impl Search {
                 &theme.name,
                 edit_distance(&theme.name, &self.name)
             );
-            if edit_distance(&theme.name, &self.name) <= 3 {
+            if Search::fuzzy_matcher_match(&theme.name, &self.name) {
                 let current = match theme.current {
                     Some(true) => "Current: ".bright_yellow().bold(),
                     _ => "".white(),
@@ -63,5 +65,45 @@ impl Search {
         }
 
         Ok(())
+    }
+
+    #[allow(dead_code)]
+    // Performs a match using edit_distance.
+    fn edit_distance_match(a: &str, b: &str) -> bool {
+        if edit_distance(a, b) <= 3 {
+            return true;
+        }
+        false
+    }
+
+    // Performs a using fuzzy matcher.
+    fn fuzzy_matcher_match(a: &str, b: &str) -> bool {
+        let matcher = SkimMatcherV2::default();
+        matcher.fuzzy_match(a, b).is_some()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_match() {
+        // Temporary, to demonstrate mismatch. Will be removed later.
+        assert!(!Search::edit_distance_match("apple pie", "apple"));
+        assert!(!Search::edit_distance_match("apple pie", "pie"));
+        assert!(!Search::edit_distance_match("Windows XP", "windows"));
+        assert!(!Search::edit_distance_match("Windows XP", "xp"));
+        assert!(!Search::edit_distance_match("Windows XP", "zinbows"));
+        assert!(!Search::edit_distance_match("Soothe", "soo"));
+        assert!(Search::edit_distance_match("Soothe", "soohe"));
+
+        assert!(Search::fuzzy_matcher_match("apple pie", "apple"));
+        assert!(Search::fuzzy_matcher_match("apple pie", "pie"));
+        assert!(Search::fuzzy_matcher_match("Windows XP", "xp"));
+        assert!(Search::fuzzy_matcher_match("Windows XP", "windows"));
+        assert!(!Search::fuzzy_matcher_match("Windows XP", "zinbows"));
+        assert!(Search::fuzzy_matcher_match("Soothe", "soo"));
+        assert!(Search::fuzzy_matcher_match("Soothe", "soohe"));
     }
 }
