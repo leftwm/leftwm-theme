fe66ebf341d3221943b611912fb52f1aeddde33e
mautam <mautam@usa.com>
2021-10-31 13:19:08 -0500

    Feature (uninstall): Add --noconfirm flag, closes #41

--- src/operations/uninstall.rs
+++ src/operations/uninstall.rs
@@ -10,7 +10,11 @@ use std::path::Path;
 
 #[derive(Clap, Debug)]
 pub struct Uninstall {
+    /// Name of theme to uninstall
     pub name: String,
+    /// Whether to prompt for confirmation
+    #[clap(long)]
+    pub noconfirm: bool,
 }
 
 impl Uninstall {
@@ -28,14 +32,16 @@ impl Uninstall {
         };
         if let Some(directory) = theme.directory {
             let path = Path::new(&directory);
-            if yes_or_no(&format!(
+            if self.noconfirm || yes_or_no(&format!(
                 "    Are you sure you want to uninstall this theme, located at {}?",
                 path.to_str().unwrap_or("Unknown location")
             )) {
                 fs::remove_dir_all(path)?;
                 match theme.source {
                     Some(source) => match Theme::find_mut(&mut config, &self.name, &source) {
-                        Some(target_theme) => target_theme.directory = None,
+                        Some(target_theme) => {target_theme.directory = None;
+                           println!("{}", format!("Theme {} uninstalled.",&self.name).green().bold()); 
+                        },
                         None => return Err(LeftError::from("Could not find theme")),
                     },
                     None => return Err(LeftError::from("No source found")),
